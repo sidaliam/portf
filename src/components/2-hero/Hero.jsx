5bb0d73a746ecf50d5120ee445e922e35ed8ba44
sidaliam <sidaliamo18@gmail.com>
2024-10-29 00:10:24 +0100

    add photos

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -1,22 +1,69 @@
 import "./hero.css";
 import Lottie from "lottie-react";
 import webdev from "../../../public/animation/Animation - 1712669223779.json";
+
 const Hero = () => {
   return (
     <section className="hero flex">
-      <div className="left-section  ">
+      <div className="left-section">
         <div className="parent-avatar flex">
-          <img src="./me.jpg" className="avatar" alt="" />
+          <img src="./me.jpg" className="avatar" alt="Avatar" />
           <div className="icon-verified"></div>
         </div>
 
         <h1 className="title">
           Amoura Sidali <br />
-          Full stack developer
+          Full Stack Developer
         </h1>
         <p className="sub-title">
-          Hello,I’m Amoura Sidali, a Full Stack developer.
+          <p className="sub-title">
+            Hello, I’m Amoura Sidali, a Full Stack Developer and freelance
+            developer with 5 years of experience. I specialize in web
+            development with HTML, CSS, ReactJS, and GitHub, and I have
+            expertise in data analysis using Python as a trained data scientist.
+          </p>
         </p>
+
+        <div className="tech-icons flex">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+            alt="HTML Logo"
+            className="tech-logo"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+            alt="CSS Logo"
+            className="tech-logo"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+            alt="React Logo"
+            className="tech-logo"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg"
+            alt="MongoDB Logo"
+            className="tech-logo"
+          />
+
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
+            alt="Node.js Logo"
+            className="tech-logo"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
+            alt="Python Logo"
+            className="tech-logo"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+            alt="GitHub Logo"
+            className="tech-logo"
+          />
+        </div>
+        <br />
+        <br />
 
         <div className="all-icons flex">
           <div className="icon icon-twitter"></div>
@@ -27,10 +74,9 @@ const Hero = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <i className="fab fa-github"></i> 
+              <i className="fab fa-github"></i>
             </a>
           </div>
-
           <div className="icon icon-linkedin"></div>
         </div>
       </div>
